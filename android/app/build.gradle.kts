91502b00dbec14f5691feeb4df6c68c435206c32
kurtborg <kurt.borg.f32447@gmail.com>
2026-01-28 14:52:52 +0100

    more items added to connect with firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -41,6 +43,8 @@ android {
 dependencies {
     // ✅ Desugaring library
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+
+
 }
 
 flutter {
